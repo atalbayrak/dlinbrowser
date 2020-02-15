efebd59821f621264fa6f42480e518aca8c6125c
taha <ahmetahalbayrak@gmail.com>
2020-02-15 20:38:37 +0300

    Changed

--- main.py
+++ main.py
@@ -1,97 +1,18 @@
 import streamlit as st
-import numpy as np
-
-import torch
-import torch.nn as nn
-import string
+from name_generator import NameGenerator
 import random
+import string
 
-all_categories= ['hobbit', 'ainur', 'dwarf', 'man', 'elf']
-n_categories = len(all_categories)
-
-all_letters = string.ascii_letters + " .,;'-"
-n_letters = len(all_letters) + 1
-
-max_length = 20
-
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(RNN, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
-        self.o2o = nn.Linear(hidden_size + output_size, output_size)
-        self.dropout = nn.Dropout(0.1)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, category, input, hidden):
-        input_combined = torch.cat((category, input, hidden), 1)
-        hidden = self.i2h(input_combined)
-        output = self.i2o(input_combined)
-        output_combined = torch.cat((hidden, output), 1)
-        output = self.o2o(output_combined)
-        output = self.dropout(output)
-        output = self.softmax(output)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
-
-
-# One-hot vector for category
-def categoryTensor(category):
-    li = all_categories.index(category)
-    tensor = torch.zeros(1, n_categories)
-    tensor[0][li] = 1
-    return tensor
-
-# One-hot matrix of first to last letters (not including EOS) for input
-def inputTensor(line):
-    tensor = torch.zeros(len(line), 1, n_letters)
-    for li in range(len(line)):
-        letter = line[li]
-        tensor[li][0][all_letters.find(letter)] = 1
-    return tensor
-
-# LongTensor of second letter to end (EOS) for target
-def targetTensor(line):
-    letter_indexes = [all_letters.find(line[li]) for li in range(1, len(line))]
-    letter_indexes.append(n_letters - 1) # EOS
-    return torch.LongTensor(letter_indexes)
-
-def generate(category, start_letter='A'):
-    with torch.no_grad():  # no need to track history in sampling
-        category_tensor = categoryTensor(category)
-        input = inputTensor(start_letter)
-        hidden = rnn.initHidden()
-
-        output_name = start_letter
-
-        for i in range(max_length):
-            output, hidden = rnn(category_tensor, input[0], hidden)
-            topv, topi = output.topk(1)
-            topi = topi[0][0]
-            if topi == n_letters - 1:
-                break
-            else:
-                letter = all_letters[topi]
-                output_name += letter
-            input = inputTensor(letter)
-
-        return output_name
-
-rnn = RNN(n_letters, 128, n_letters)
-rnn.load_state_dict(torch.load('model/name_lotr.pth'))
-rnn.eval()
 st.title('Name Generator')
 """Whether you're trying to write a character
 list for an epic voyage, creating a group of witches
 for a children's book, or just trying to name your pet
 unicorn, we'll find the name for you."""
+
+rnn = NameGenerator()
 race = st.selectbox(label="Race", options=('Man', 'Elf', 'Dwarf', 'Hobbit', 'Ainur'))
 username = st.text_input(label="Starting Characters" ,value='', key=None, type='default')
 
 if username == '':
     username = random.choice(string.ascii_letters).upper()
-st.text('Generated name is : ' + generate(race.lower(),username))
+st.text('Generated name is : ' + rnn.generate_name(race,username))
---
+++ name_generator.py
@@ -0,0 +1,90 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import string
+import random
+
+all_categories= ['hobbit', 'ainur', 'dwarf', 'man', 'elf']
+n_categories = len(all_categories)
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters) + 1
+
+max_length = 20
+name_lotr_pth = 'model/name_lotr.pth'
+
+class NameGenerator():
+    def __init__(self):
+        rnn = RNN(n_letters, 128, n_letters)
+        rnn.load_state_dict(torch.load(name_lotr_pth))
+        rnn.eval()
+        self.rnn = rnn
+    def generate_name(self,race, name):
+        return generate(self.rnn,race.lower(),name)
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+# One-hot vector for category
+def categoryTensor(category):
+    li = all_categories.index(category)
+    tensor = torch.zeros(1, n_categories)
+    tensor[0][li] = 1
+    return tensor
+
+# One-hot matrix of first to last letters (not including EOS) for input
+def inputTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li in range(len(line)):
+        letter = line[li]
+        tensor[li][0][all_letters.find(letter)] = 1
+    return tensor
+
+# LongTensor of second letter to end (EOS) for target
+def targetTensor(line):
+    letter_indexes = [all_letters.find(line[li]) for li in range(1, len(line))]
+    letter_indexes.append(n_letters - 1) # EOS
+    return torch.LongTensor(letter_indexes)
+
+def generate(rnn, category, start_letter='A'):
+    with torch.no_grad():  # no need to track history in sampling
+        category_tensor = categoryTensor(category)
+        input = inputTensor(start_letter)
+        hidden = rnn.initHidden()
+
+        output_name = start_letter
+
+        for i in range(max_length):
+            output, hidden = rnn(category_tensor, input[0], hidden)
+            topv, topi = output.topk(1)
+            topi = topi[0][0]
+            if topi == n_letters - 1:
+                break
+            else:
+                letter = all_letters[topi]
+                output_name += letter
+            input = inputTensor(letter)
+
+        return output_name
